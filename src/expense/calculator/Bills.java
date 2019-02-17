09c877dc77471b75e570f351c7c9f7e9461f879a
sunnyjasti <sunnyjasti15@gmail.com>
2019-02-17 10:36:23 -0500

    Bills class getter,setter created

--- src/expense/calculator/Bills.java
+++ src/expense/calculator/Bills.java
@@ -16,4 +16,51 @@ public class Bills {
     String bill_date;
     String bill_member;
     
+  public void setBName(String btyp)
+    {
+        bill_type = btyp;
+    }
+     public void setBDes(String bdescription)
+    {
+        bill_description = bdescription;
+    }
+      public void setBAmount(double bamount)
+    {
+        bill_amount = bamount;
+    } 
+      public void setBDate(String bdate)
+    {
+        bill_date = bdate;
+    }
+     public void setBMember(String bmember)
+    {
+        bill_member = bmember;
+    }
+      
+    public String getBName()
+      {
+          return bill_type;
+         
+      }
+       public String getBDes()
+      {
+          return bill_description;
+         
+      }
+           public double getBAmount()
+      {
+          return bill_amount;
+         
+      }
+           public String getBDate()
+      {
+          return bill_date;
+         
+      }
+       public String getBMember()
+      {
+          return bill_member;
+         
+      }
+          
 }
