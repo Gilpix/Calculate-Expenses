901b65073079a6cce195d6c1debcc554223d21d2
sunnyjasti <sunnyjasti15@gmail.com>
2019-02-17 10:44:36 -0500

    Group Member class attributes,getter,setter created

--- src/expense/calculator/GroupMember.java
+++ src/expense/calculator/GroupMember.java
@@ -10,5 +10,27 @@ package expense.calculator;
  * @author temp
  */
 public class GroupMember {
+    String member_name;
+    String member_email;
+    
+    public void setMName(String mname)
+    {
+        member_name = mname;
+    }
+     public void setMEmail(String memail)
+    {
+        member_email = memail;
+    }
+      
+    public String getMName()
+      {
+          return member_name;
+         
+      }
+       public String getMEmail()
+      {
+          return member_email;
+         
+      }
     
 }
