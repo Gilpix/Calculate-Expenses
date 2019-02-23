24db59fbb6ff26b93e06cf1eb60ab5a4b23dddc9
sunnyjasti <sunnyjasti15@gmail.com>
2019-02-23 11:23:38 -0500

    all class invoked in main

--- src/expense/calculator/ExpenseCalculator.java
+++ src/expense/calculator/ExpenseCalculator.java
@@ -18,10 +18,52 @@ public class ExpenseCalculator {
         // TODO code application logic here\
         
         
-        User u = new User(9923,"Kuldeep"," ","kul@gmail.com");
+        Person p = new Person("Kuldeep"," ","kul@gmail.com");
+        p.setFirstName("Kamal");
+        p.setLastName("Padda");
+        p.setPEmail("kamal@1212");
         
-     
-         System.out.println("user Current ID : "+u.getId());
+        
+        
+        
+        
+        User u = new User(9923,"Kuldeep"," ","kul@gmail.com");
+        u.setFirstName("Anju");
+        u.setLastName("Paul");
+        System.out.println("user Current ID : "+u.getId());
+        
+        
+        GroupAdmin ga = new GroupAdmin("Apartment","Kuldeep"," ","kul@gmail.com");
+        ga.setAdminGroupName("Niagra Falls");
+        ga.setFirstName("Raminder");
+        ga.setLastName("Singh");
+        ga.setPEmail("raminder@gmail.com");
+        
+        
+        
+        GroupMember gm=new GroupMember("Kamaldeep","kamal@gmail.com");
+        gm.setMName("Kamal Padda");
+        gm.setMEmail("kamala@gmail.com");
+        
+        
+        
+        Friends f= new Friends("Kamaldeep","kamal@gmail.com",99238651);
+        f.setFname("Kul Singh");
+        f.setFemail("kul@gmail.com");
+        f.setFmobile(514292151);
+        
+        
+        Bills b=new Bills("Grocery","Niagra fall eating bill",1544,"2019-02-21","Kamala");
+        b.setBName("travel");
+        b.setBDes("Travel bill for Niagra Falls");
+        b.setBAmount(1521);
+        b.setBMember("Kamala");
+        b.setBDate("2019-02-15");
+        
+        Details d=new Details("2019-02-15");
+        d.setDdate("2019-02-15");
+        
+        
         
     }
     
