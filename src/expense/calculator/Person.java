62d6576d381707f35f5a460329a0f3fddd64b03e
sunnyjasti <sunnyjasti15@gmail.com>
2019-02-23 13:11:17 -0500

    id created

--- src/expense/calculator/Person.java
+++ src/expense/calculator/Person.java
@@ -4,17 +4,29 @@
  * and open the template in the editor.
  */
 package expense.calculator;
+import java.util.Calendar;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 /**
  *
  * @author temp
  */
 public class Person {
-    String lastName, firstName, pEmail;
+    String lastName, firstName, pEmail,id;
     
-    public Person(String fName, String lName, String email)
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public Person(String fName, String lName, String email, Calendar newid)
     {
-        
+        id = getUserCurrentId(fName,newid);
         firstName = fName;
         lastName = lName;
         pEmail = email;
@@ -48,4 +60,58 @@ public class Person {
         return pEmail;
     }
     
+    
+    public  void personDisplay()
+    {
+         
+        System.out.println("Person First Name : "+getFName());
+        System.out.println("Person Last Name : "+getLName());
+        System.out.println("Person Email : "+getPEmail());
+    }
+    
+     public String getUserCurrentId(String fname, Calendar id)
+    {
+        Calendar calendar = Calendar.getInstance();
+    DateFormat dateFormat = new SimpleDateFormat("yy-MMM-dd");
+	Calendar cal = Calendar.getInstance();
+       
+          char f;
+          char l;
+              char hyphen='-';
+        if(getFName().charAt(0)==' ')
+            f='X';
+            else
+             f=getFName().charAt(0);
+     
+         if(getLName().charAt(0)==' ')
+            l='X';
+            else
+             l=getLName().charAt(0);
+        
+         
+         f = Character.toUpperCase(f);
+         l = Character.toUpperCase(l);
+         
+         String dte= dateFormat.format(cal.getTime());
+         String year,month,oe;
+         int index=0;
+         index =dte.indexOf("-");
+         year=dte.substring(0,index);
+         index++;
+         int sv =index;
+         index = dte.indexOf("-", index);
+          month=dte.substring(sv,index).toUpperCase();
+          index++;
+         
+          oe=dte.substring(index).toUpperCase();
+          int oE =Integer.parseInt(oe);
+          if(oE%2==0)
+              oe="E";
+          else
+              oe="O";  
+          
+          String finalId =year+hyphen+month+hyphen+oe+hyphen+f+l;
+         return  finalId;
+
+    }
 }
