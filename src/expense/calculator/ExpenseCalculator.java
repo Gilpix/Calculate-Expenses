faf0a7b7ac66de10a6b62366adf3e979c77d2820
sunnyjasti <sunnyjasti15@gmail.com>
2019-02-23 09:31:39 -0500

    user class invoked in main

--- src/expense/calculator/ExpenseCalculator.java
+++ src/expense/calculator/ExpenseCalculator.java
@@ -15,7 +15,14 @@ public class ExpenseCalculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here\
+        
+        
+        User u = new User(9923,"Kuldeep","Singh","kul@gmail.com");
+        char f=u.getFName().charAt(0);
+        //char l=u.getLName().charAt(0);
+         System.out.println("Today's date : "+u.getcurrentDate()+"-"+f);
+        
     }
     
 }
