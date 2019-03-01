8823d7b1fb64de36a0ffee992d0fd244d660efe6
APanj <APanj@192.168.1.9>
2019-02-28 21:10:35 -0500

    calender class called in group member class

--- src/expense/calculator/GroupMember.java
+++ src/expense/calculator/GroupMember.java
@@ -5,11 +5,20 @@
  */
 package expense.calculator;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /**
  *
  * @author temp
  */
 public class GroupMember {
+    
+    Calendar calendar = Calendar.getInstance();
+    DateFormat dateFormat = new SimpleDateFormat("yy-MMM-dd");
+    Calendar cal = Calendar.getInstance();
+    
     String member_name;
     String member_email;
     
