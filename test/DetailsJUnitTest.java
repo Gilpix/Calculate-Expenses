50c8c6a737d27938ea0b8acc240270169e9fdbe9
Karan <temp@E21023.CEGEPGIM.LCL>
2019-03-02 16:27:44 -0500

    created jUnit Test for DetailsId function

---
+++ test/DetailsJUnitTest.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Calendar;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author temp
+ */
+public class DetailsJUnitTest {
+    
+    public DetailsJUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void getDetailsCurrentId() {
+        
+        
+         System.out.println("Test number 1 for getDetailsCurrentId method");
+         String firstName = "XX";
+        
+         
+         Calendar cal = Calendar.getInstance();
+         
+         String expectedResult="19-MAR-E-XX";
+         
+        
+         String realResult=expense.calculator.Details.getDetailsCurrentId(cal);
+         
+         assertEquals(expectedResult,realResult);
+    
+    }
+}
