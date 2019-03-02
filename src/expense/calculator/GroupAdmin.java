03759b66401ffc8c907c71521d33803797d6e6a9
sunnyjasti <sunnyjasti15@gmail.com>
2019-03-02 10:45:45 -0500

    GroupsAdmin Class Id created and getUserId function created

--- src/expense/calculator/GroupAdmin.java
+++ src/expense/calculator/GroupAdmin.java
@@ -5,6 +5,8 @@
  */
 package expense.calculator;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 /**
@@ -14,6 +16,11 @@ import java.util.Calendar;
 public class GroupAdmin extends Person {
 
     String groupName;
+     private String id;
+    
+     Calendar calendar = Calendar.getInstance();
+    DateFormat dateFormat = new SimpleDateFormat("yy-MMM-dd");
+    Calendar cal = Calendar.getInstance();
 
     public GroupAdmin(String gName, String fName, String lName, String pEmail,Calendar newId)
     {
@@ -32,12 +39,62 @@ public class GroupAdmin extends Person {
     
      public  void groupAdminDisplay()
     {
+        System.out.println("Admin Id :"+getId());
          System.out.println("Group Admin Group name : "+getAdminGroupName());
         System.out.println("Group Admin Name : "+getFName()+" "+getLName());
         System.out.println("Group Admin Email : "+getPEmail());
     }
+     
+     
+         public String getId() {
+        return getUserCurrentId();
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+     
+     
+      public String getUserCurrentId()
+    {
+       
+          char f;
+          char l;
+              char hyphen='-';
+        if(getAdminGroupName().charAt(0)==' ')
+            f='X';
+            else
+             f=getAdminGroupName().charAt(0);
+     
+        int index1 =getAdminGroupName().indexOf(" ");
+         if(getAdminGroupName().charAt(index1+1)==' ')
+            l='X';
+            else
+             l=getAdminGroupName().charAt(index1+1);
+        
+         
+         f = Character.toUpperCase(f);
+         l = Character.toUpperCase(l);
+         
+         String dte= dateFormat.format(cal.getTime());
+         String year,month,oe;
+         int index=0;
+         index =dte.indexOf("-");
+         year=dte.substring(0,index);
+         index++;
+         int sv =index;
+         index = dte.indexOf("-", index);
+          month=dte.substring(sv,index).toUpperCase();
+          index++;
+         
+          oe=dte.substring(index).toUpperCase();
+          int oE =Integer.parseInt(oe);
+          if(oE%2==0)
+              oe="E";
+          else
+              oe="O";  
+         return  year+hyphen+month+hyphen+oe+hyphen+f+l;
 
-
+    }
 
 }
