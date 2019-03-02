80c33cd942d5fb5ec489a872fdf53ee6303d0e9d
1895250 <1895250@E21025.CEGEPGIM.LCL>
2019-03-02 16:23:47 -0500

    updated J Unit Test for member class function

---
+++ test/GroupMemberJUnitTest.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Calendar;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 1895250
+ */
+public class GroupMemberJUnitTest {
+    
+    public GroupMemberJUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+     public void getGroupMemberCurrentId() {
+        
+        
+         System.out.println("Test for getGroupMemberCurrentId method in bill class");
+         String name = "Anju Paul";
+         Calendar cal = Calendar.getInstance();
+         
+         String expectedResult="19-MAR-E-AP";
+         
+        
+         String realResult=expense.calculator.GroupMember.getGroupMemberCurrentId(name);
+         
+         assertEquals(expectedResult,realResult);
+     }
+}
