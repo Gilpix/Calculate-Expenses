f60b842764867791ef6209e318171312365c22e1
Gilpix <kuldeepsingh5005@gmail.com>
2019-03-03 05:13:32 -0500

    User class fun JUnit test updated

--- test/UserJUnitTest.java
+++ test/UserJUnitTest.java
@@ -47,12 +47,9 @@ public class UserJUnitTest {
          System.out.println("Test number 1 for getUserCurrentId method");
          String firstName = "Kuldeep";
          String lastName ="Singh";
-         char fN=lastName.charAt(0);
-         char lN=lastName.charAt(0);
          
          String expectedResult="19-MAR-O-KS";
          
-        
          String realResult=expense.calculator.User.getUserCurrentId(firstName,lastName);
          
          assertEquals(expectedResult,realResult);
