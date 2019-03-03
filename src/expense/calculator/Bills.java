6981ba006e1819d4d162f71f0e99442553a4dad2
1895250 <1895250@E21025.CEGEPGIM.LCL>
2019-03-03 09:03:03 -0500

    format the code of bill class and group member class

--- src/expense/calculator/Bills.java
+++ src/expense/calculator/Bills.java
@@ -14,155 +14,147 @@ import java.util.Calendar;
  * @author temp
  */
 public class Bills {
-    
-   
-    
+
     String bill_type;
     String bill_description;
     double bill_amount;
     String bill_date;
     String bill_member;
-    
+
     private String id;
-    
+
     private User us;
     private GroupAdmin gad;
     private GroupMember gmem;
-    
-    public Bills(String bt,String bdes,double bamt,String bdt,String bmem)
-    {
+
+    public Bills(String bt, String bdes, double bamt, String bdt, String bmem) {
         bill_type = bt;
         bill_description = bdes;
         bill_amount = bamt;
         bill_date = bdt;
         bill_member = bmem;
-        id= getBillCurrentId(bmem);
-        
+        id = getBillCurrentId(bmem);
+
     }
-    
-    public void User(User newUsr)
-    {
+
+    public void User(User newUsr) {
         this.us = newUsr;
     }
-    public void GroupAdmin(GroupAdmin newgad)
-    {
+
+    public void GroupAdmin(GroupAdmin newgad) {
         this.gad = newgad;
     }
-    public void GroupMember(GroupMember newgmem)
-    {
+
+    public void GroupMember(GroupMember newgmem) {
         this.gmem = newgmem;
     }
-    
-  public void setBName(String btyp)
-    {
+
+    public void setBName(String btyp) {
         bill_type = btyp;
     }
-     public void setBDes(String bdescription)
-    {
+
+    public void setBDes(String bdescription) {
         bill_description = bdescription;
     }
-      public void setBAmount(double bamount)
-    {
+
+    public void setBAmount(double bamount) {
         bill_amount = bamount;
-    } 
-      public void setBDate(String bdate)
-    {
+    }
+
+    public void setBDate(String bdate) {
         bill_date = bdate;
     }
-     public void setBMember(String bmember)
-    {
+
+    public void setBMember(String bmember) {
         bill_member = bmember;
     }
-      
-    public String getBName()
-      {
-          return bill_type;
-         
-      }
-       public String getBDes()
-      {
-          return bill_description;
-         
-      }
-           public double getBAmount()
-      {
-          return bill_amount;
-         
-      }
-           public String getBDate()
-      {
-          return bill_date;
-         
-      }
-       public String getBMember()
-      {
-          return bill_member;
-         
-      }
-         
-       public static String getBillCurrentId(String bMnane)
-    { 
+
+    public String getBName() {
+        return bill_type;
+
+    }
+
+    public String getBDes() {
+        return bill_description;
+
+    }
+
+    public double getBAmount() {
+        return bill_amount;
+
+    }
+
+    public String getBDate() {
+        return bill_date;
+
+    }
+
+    public String getBMember() {
+        return bill_member;
+
+    }
+
+    public static String getBillCurrentId(String bMnane) {
         Calendar calendar = Calendar.getInstance();
-    DateFormat dateFormat = new SimpleDateFormat("yy-MMM-dd");
-    Calendar cal = Calendar.getInstance();
-        
+        DateFormat dateFormat = new SimpleDateFormat("yy-MMM-dd");
+        Calendar cal = Calendar.getInstance();
+
         char f;
-          char l;
-              char hyphen='-';
-        if(bMnane.charAt(0)==' ')
-            f='X';
-            else
-             f=bMnane.charAt(0);
-     
-       
-        if(bMnane.indexOf(" ")>0 &&bMnane.indexOf(" ")<bMnane.length())
-        {
-             int index1 =bMnane.indexOf(" ");
-                 l=bMnane.charAt(index1+1);
+        char l;
+        char hyphen = '-';
+        if (bMnane.charAt(0) == ' ') {
+            f = 'X';
+        } else {
+            f = bMnane.charAt(0);
         }
-        else
-            l='X';
-  
-         f = Character.toUpperCase(f);
-         l = Character.toUpperCase(l);
-         
-         String dte= dateFormat.format(cal.getTime());
-         String year,month,oe;
-         int index=0;
-         index =dte.indexOf("-");
-         year=dte.substring(0,index);
-         index++;
-         int sv =index;
-         index = dte.indexOf("-", index);
-          month=dte.substring(sv,index).toUpperCase();
-          index++;
-         
-          oe=dte.substring(index).toUpperCase();
-          int oE =Integer.parseInt(oe);
-          if(oE%2==0)
-              oe="E";
-          else
-              oe="O";  
-         return  year+hyphen+month+hyphen+oe+hyphen+f+l;
+
+        if (bMnane.indexOf(" ") > 0 && bMnane.indexOf(" ") < bMnane.length()) {
+            int index1 = bMnane.indexOf(" ");
+            l = bMnane.charAt(index1 + 1);
+        } else {
+            l = 'X';
+        }
+
+        f = Character.toUpperCase(f);
+        l = Character.toUpperCase(l);
+
+        String dte = dateFormat.format(cal.getTime());
+        String year, month, oe;
+        int index = 0;
+        index = dte.indexOf("-");
+        year = dte.substring(0, index);
+        index++;
+        int sv = index;
+        index = dte.indexOf("-", index);
+        month = dte.substring(sv, index).toUpperCase();
+        index++;
+
+        oe = dte.substring(index).toUpperCase();
+        int oE = Integer.parseInt(oe);
+        if (oE % 2 == 0) {
+            oe = "E";
+        } else {
+            oe = "O";
+        }
+        return year + hyphen + month + hyphen + oe + hyphen + f + l;
 
     }
 
     public String getId() {
         return id;
-       // return getBillCurrentId();
+        // return getBillCurrentId();
     }
 
     public void setId(String id) {
         this.id = id;
     }
-    
-    public  void BillsDisplay()
-    {
-        System.out.println("Bill Current ID : "+getId());
-        System.out.println("Bill Type : "+getBName());
-        System.out.println("Bill Description : "+getBDes());
-        System.out.println("Bill Amount : "+getBAmount());
-        System.out.println("Bill Creation Date : "+getBDate());
-        System.out.println("Bill Member : "+getBMember());
+
+    public void BillsDisplay() {
+        System.out.println("Bill Current ID : " + getId());
+        System.out.println("Bill Type : " + getBName());
+        System.out.println("Bill Description : " + getBDes());
+        System.out.println("Bill Amount : " + getBAmount());
+        System.out.println("Bill Creation Date : " + getBDate());
+        System.out.println("Bill Member : " + getBMember());
     }
 }
--- src/expense/calculator/GroupMember.java
+++ src/expense/calculator/GroupMember.java
@@ -14,95 +14,89 @@ import java.util.Calendar;
  * @author temp
  */
 public class GroupMember {
-    
-    
-    
+
     String member_name;
     String member_email;
-    
+
     private String id;
-    
+
     private Groups gp;
     private GroupAdmin gad;
-    
-    public GroupMember(String mn,String me)
-    {
+
+    public GroupMember(String mn, String me) {
         member_name = mn;
         member_email = me;
-        id=getGroupMemberCurrentId(mn);
+        id = getGroupMemberCurrentId(mn);
     }
-    public void Groups(Groups newgp)
-    {
+
+    public void Groups(Groups newgp) {
         this.gp = newgp;
     }
-    public void GroupAdmin(GroupAdmin newgad)
-    {
+
+    public void GroupAdmin(GroupAdmin newgad) {
         this.gad = newgad;
     }
-    
-    public void setMName(String mname)
-    {
+
+    public void setMName(String mname) {
         member_name = mname;
     }
-     public void setMEmail(String memail)
-    {
+
+    public void setMEmail(String memail) {
         member_email = memail;
     }
-      
-    public String getMName()
-      {
-          return member_name;
-         
-      }
-       public String getMEmail()
-      {
-          return member_email;
-         
-      }
-       
-        public static String getGroupMemberCurrentId(String gMname)
-    {
+
+    public String getMName() {
+        return member_name;
+
+    }
+
+    public String getMEmail() {
+        return member_email;
+
+    }
+
+    public static String getGroupMemberCurrentId(String gMname) {
         Calendar calendar = Calendar.getInstance();
-    DateFormat dateFormat = new SimpleDateFormat("yy-MMM-dd");
-    Calendar cal = Calendar.getInstance();
+        DateFormat dateFormat = new SimpleDateFormat("yy-MMM-dd");
+        Calendar cal = Calendar.getInstance();
         char f;
-          char l;
-              char hyphen='-';
-        if(gMname.charAt(0)==' ')
-            f='X';
-            else
-             f=gMname.charAt(0);
-     
-       
-        if(gMname.indexOf(" ")>0 &&gMname.indexOf(" ")<gMname.length())
-        {
-             int index1 =gMname.indexOf(" ");
-                 l=gMname.charAt(index1+1);
+        char l;
+        char hyphen = '-';
+        if (gMname.charAt(0) == ' ') {
+            f = 'X';
+        } else {
+            f = gMname.charAt(0);
         }
-        else
-            l='X';
-  
-         f = Character.toUpperCase(f);
-         l = Character.toUpperCase(l);
-         
-         String dte= dateFormat.format(cal.getTime());
-         String year,month,oe;
-         int index=0;
-         index =dte.indexOf("-");
-         year=dte.substring(0,index);
-         index++;
-         int sv =index;
-         index = dte.indexOf("-", index);
-          month=dte.substring(sv,index).toUpperCase();
-          index++;
-         
-          oe=dte.substring(index).toUpperCase();
-          int oE =Integer.parseInt(oe);
-          if(oE%2==0)
-              oe="E";
-          else
-              oe="O";  
-         return  year+hyphen+month+hyphen+oe+hyphen+f+l;
+
+        if (gMname.indexOf(" ") > 0 && gMname.indexOf(" ") < gMname.length()) {
+            int index1 = gMname.indexOf(" ");
+            l = gMname.charAt(index1 + 1);
+        } else {
+            l = 'X';
+        }
+
+        f = Character.toUpperCase(f);
+        l = Character.toUpperCase(l);
+
+        String dte = dateFormat.format(cal.getTime());
+        String year, month, oe;
+        int index = 0;
+        index = dte.indexOf("-");
+        year = dte.substring(0, index);
+        index++;
+        int sv = index;
+        index = dte.indexOf("-", index);
+        month = dte.substring(sv, index).toUpperCase();
+        index++;
+
+        oe = dte.substring(index).toUpperCase();
+        int oE = Integer.parseInt(oe);
+        if (oE % 2 == 0) {
+            oe = "E";
+        } else {
+            oe = "O";
+        }
+        return year + hyphen + month + hyphen + oe + hyphen + f + l;
     }
 
     public String getId() {
@@ -112,12 +106,11 @@ public class GroupMember {
     public void setId(String id) {
         this.id = id;
     }
-    
-    public  void GroupMemberDisplay()
-    {
-        System.out.println("Group Member ID : "+getId());
-        System.out.println("Group Member Name : "+getMName());
-        System.out.println("group Member Email : "+getMEmail());
+
+    public void GroupMemberDisplay() {
+        System.out.println("Group Member ID : " + getId());
+        System.out.println("Group Member Name : " + getMName());
+        System.out.println("group Member Email : " + getMEmail());
     }
-    
+
 }
